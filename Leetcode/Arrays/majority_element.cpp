5530df881b700892e1198bf253e464d527833899
Sanvee Karandikar <sanvee.karandikar@comprinno.net>
2026-01-21 10:55:07 +0530

    Added majority element solution in C++

---
+++ Leetcode/Arrays/majority_element.cpp
@@ -0,0 +1,21 @@
+Problem : Majority Element
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Constraints:
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+The input is generated such that a majority element will exist in the array.
+
+Code :
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return nums[n/2];
+    }
+};
