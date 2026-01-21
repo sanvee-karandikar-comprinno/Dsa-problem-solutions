b070243d26d644a0f455dc7e11a57e5c8dc81704
Sanvee Karandikar <sanvee.karandikar@comprinno.net>
2026-01-21 10:21:53 +0530

    Added Two Sum solution in C++

---
+++ Leetcode/Arrays/two_sum.cpp
@@ -0,0 +1,34 @@
+Problem : Two sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+ 
+
+Example:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+
+Constraints:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Code :
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; 
+    }
+};  
